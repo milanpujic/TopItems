b2bc6e0768a38e97a623f65f09be561625c75b9c
milanpujic <milan.pujic@soprex.com>
2016-09-28 11:07:29 +0200

    Update ContentController.hh

--- src/Controllers/ContentController.hh
+++ src/Controllers/ContentController.hh
@@ -3,54 +3,54 @@
 namespace TopItems\Controllers;
 
 
-  use Plenty\Plugin\Controller;
-  use Plenty\Plugin\Templates\Twig;
-  use Plenty\Modules\Item\DataLayer\Contracts\ItemDataLayerRepositoryContract;
+    use Plenty\Plugin\Controller;
+    use Plenty\Plugin\Templates\Twig;
+    use Plenty\Modules\Item\DataLayer\Contracts\ItemDataLayerRepositoryContract;
 
-  class ContentController extends Controller
-  {
-      public function showTopItems(Twig $twig, ItemDataLayerRepositoryContract $itemRepository):string
-      {
-          $itemColumns = [
-              'itemDescription' => [
-                  'name1',
-                  'description'
-              ],
-              'variationBase' => [
-                  'id'
-              ],
-              'variationRetailPrice' => [
-                  'price'
-              ],
-              'variationImageList' => [
-                  'path',
-                  'cleanImageName'
-              ]
-          ];
+    class ContentController extends Controller
+    {
+        public function showTopItems(Twig $twig, ItemDataLayerRepositoryContract $itemRepository):string
+        {
+            $itemColumns = [
+                'itemDescription' => [
+                    'name1',
+                    'description'
+                ],
+                'variationBase' => [
+                    'id'
+                ],
+                'variationRetailPrice' => [
+                    'price'
+                ],
+                'variationImageList' => [
+                    'path',
+                    'cleanImageName'
+                ]
+            ];
 
-          $itemFilter = [
-              'itemBase.isStoreSpecial' => [
-                  'shopAction' => [3]
-              ]
-          ];
+            $itemFilter = [
+                'itemBase.isStoreSpecial' => [
+                    'shopAction' => [3]
+                ]
+            ];
 
-          $itemParams = [
-              'language' => 'en'
-          ];
+            $itemParams = [
+                'language' => 'en'
+            ];
 
-          $resultItems = $itemRepository
-              ->search( $itemColumns, $itemFilter, $itemParams );
+            $resultItems = $itemRepository
+                ->search( $itemColumns, $itemFilter, $itemParams );
 
-          $items = array();
-          foreach ($resultItems as $item)
-          {
-              $items[] = $item;
-          }
-          $templateData = array(
-              'resultCount' => $resultItems->count(),
-              'currentItems' => $items
-          );
+            $items = array();
+            foreach ($resultItems as $item)
+            {
+                $items[] = $item;
+            }
+            $templateData = array(
+                'resultCount' => $resultItems->count(),
+                'currentItems' => $items
+            );
 
-          return $twig->render('TopItems::content.TopItems', $templateData );
-      }
-  }
+            return $twig->render('TopItems::content.TopItems', $templateData );
+        }
+    }
